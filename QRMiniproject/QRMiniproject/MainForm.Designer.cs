0b9c6272808b8431c5f2274f2eda92851692aa63
JKbin <wiken0072@naver.com>
2020-07-06 14:02:55 +0900

    2020.07.06 MainForm 로그아웃 설정

--- QRMiniproject/QRMiniproject/MainForm.Designer.cs
+++ QRMiniproject/QRMiniproject/MainForm.Designer.cs
@@ -35,24 +35,25 @@
             this.ClientManaged = new System.Windows.Forms.Button();
             this.BtnInPut = new System.Windows.Forms.Button();
             this.EmployeeManaged = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblUserID = new System.Windows.Forms.Label();
+            this.BtnLogOut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 144);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(148, 120);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(302, 18);
+            this.label1.Size = new System.Drawing.Size(254, 15);
             this.label1.TabIndex = 8;
             this.label1.Text = "QR코드를 이용한 입출고 관리시스템";
             // 
             // ProductManaged
             // 
-            this.ProductManaged.Location = new System.Drawing.Point(170, 437);
-            this.ProductManaged.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ProductManaged.Location = new System.Drawing.Point(136, 364);
             this.ProductManaged.Name = "ProductManaged";
-            this.ProductManaged.Size = new System.Drawing.Size(125, 59);
+            this.ProductManaged.Size = new System.Drawing.Size(100, 49);
             this.ProductManaged.TabIndex = 3;
             this.ProductManaged.Text = "품목관리";
             this.ProductManaged.UseVisualStyleBackColor = true;
@@ -60,10 +61,9 @@
             // 
             // BtnInOut
             // 
-            this.BtnInOut.Location = new System.Drawing.Point(378, 437);
-            this.BtnInOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnInOut.Location = new System.Drawing.Point(302, 364);
             this.BtnInOut.Name = "BtnInOut";
-            this.BtnInOut.Size = new System.Drawing.Size(125, 59);
+            this.BtnInOut.Size = new System.Drawing.Size(100, 49);
             this.BtnInOut.TabIndex = 2;
             this.BtnInOut.Text = "입.출고 현황";
             this.BtnInOut.UseVisualStyleBackColor = true;
@@ -71,10 +71,9 @@
             // 
             // BtnOutPut
             // 
-            this.BtnOutPut.Location = new System.Drawing.Point(378, 325);
-            this.BtnOutPut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnOutPut.Location = new System.Drawing.Point(302, 271);
             this.BtnOutPut.Name = "BtnOutPut";
-            this.BtnOutPut.Size = new System.Drawing.Size(125, 60);
+            this.BtnOutPut.Size = new System.Drawing.Size(100, 50);
             this.BtnOutPut.TabIndex = 4;
             this.BtnOutPut.Text = "출고관리";
             this.BtnOutPut.UseVisualStyleBackColor = true;
@@ -82,10 +81,9 @@
             // 
             // ClientManaged
             // 
-            this.ClientManaged.Location = new System.Drawing.Point(170, 325);
-            this.ClientManaged.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientManaged.Location = new System.Drawing.Point(136, 271);
             this.ClientManaged.Name = "ClientManaged";
-            this.ClientManaged.Size = new System.Drawing.Size(125, 60);
+            this.ClientManaged.Size = new System.Drawing.Size(100, 50);
             this.ClientManaged.TabIndex = 5;
             this.ClientManaged.Text = "거래처관리";
             this.ClientManaged.UseVisualStyleBackColor = true;
@@ -93,10 +91,9 @@
             // 
             // BtnInPut
             // 
-            this.BtnInPut.Location = new System.Drawing.Point(378, 229);
-            this.BtnInPut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnInPut.Location = new System.Drawing.Point(302, 191);
             this.BtnInPut.Name = "BtnInPut";
-            this.BtnInPut.Size = new System.Drawing.Size(125, 60);
+            this.BtnInPut.Size = new System.Drawing.Size(100, 50);
             this.BtnInPut.TabIndex = 6;
             this.BtnInPut.Text = "입고관리";
             this.BtnInPut.UseVisualStyleBackColor = true;
@@ -104,20 +101,50 @@
             // 
             // EmployeeManaged
             // 
-            this.EmployeeManaged.Location = new System.Drawing.Point(170, 229);
-            this.EmployeeManaged.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.EmployeeManaged.Location = new System.Drawing.Point(136, 191);
             this.EmployeeManaged.Name = "EmployeeManaged";
-            this.EmployeeManaged.Size = new System.Drawing.Size(125, 60);
+            this.EmployeeManaged.Size = new System.Drawing.Size(100, 50);
             this.EmployeeManaged.TabIndex = 7;
             this.EmployeeManaged.Text = "사원관리";
             this.EmployeeManaged.UseVisualStyleBackColor = true;
             this.EmployeeManaged.Click += new System.EventHandler(this.EmployeeManaged_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(136, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "접속아이디 :";
+            // 
+            // LblUserID
+            // 
+            this.LblUserID.AutoSize = true;
+            this.LblUserID.Location = new System.Drawing.Point(234, 32);
+            this.LblUserID.Name = "LblUserID";
+            this.LblUserID.Size = new System.Drawing.Size(67, 15);
+            this.LblUserID.TabIndex = 10;
+            this.LblUserID.Text = "에헤헤헿";
+            // 
+            // BtnLogOut
+            // 
+            this.BtnLogOut.Location = new System.Drawing.Point(193, 419);
+            this.BtnLogOut.Name = "BtnLogOut";
+            this.BtnLogOut.Size = new System.Drawing.Size(140, 70);
+            this.BtnLogOut.TabIndex = 11;
+            this.BtnLogOut.Text = "로그아웃";
+            this.BtnLogOut.UseVisualStyleBackColor = true;
+            this.BtnLogOut.Click += new System.EventHandler(this.BtnLogOut_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 599);
+            this.ClientSize = new System.Drawing.Size(531, 499);
+            this.Controls.Add(this.BtnLogOut);
+            this.Controls.Add(this.LblUserID);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ProductManaged);
             this.Controls.Add(this.BtnInOut);
@@ -125,10 +152,9 @@
             this.Controls.Add(this.ClientManaged);
             this.Controls.Add(this.BtnInPut);
             this.Controls.Add(this.EmployeeManaged);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
-            this.Padding = new System.Windows.Forms.Padding(25, 72, 25, 24);
             this.Text = "QR";
+            this.Activated += new System.EventHandler(this.MainForm_Activated);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -145,6 +171,9 @@
         private System.Windows.Forms.Button ClientManaged;
         private System.Windows.Forms.Button BtnInPut;
         private System.Windows.Forms.Button EmployeeManaged;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblUserID;
+        private System.Windows.Forms.Button BtnLogOut;
     }
 }
 
